38bd51f098a5c3c1ae7602ffb353a1def6276c1e
kafe0629 <kafe-cr-z@ezweb.ne.jp>
2026-08-27 09:36:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='odyssey-v4.4';
+const CACHE='odyssey-v4.5';
 const CORE=['./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})());});
