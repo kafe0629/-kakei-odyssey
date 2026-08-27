8d6246d188fe3510c75e794f34232a426648441f
kafe0629 <kafe-cr-z@ezweb.ne.jp>
2026-08-27 23:16:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-const CACHE='kakei-odyssey-v6.5-20260827';
+const CACHE='kakei-odyssey-v6.5.1-20260827';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
-      const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r;
-    }).catch(()=>caches.match('./index.html')));
-    return;
-  }
-  e.respondWith(fetch(e.request).then(r=>{
-    const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;
-  }).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));return;}e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));});
