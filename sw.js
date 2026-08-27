46d911df74a0daa4d1b9b3d75b04b2f379c11f59
kafe0629 <kafe-cr-z@ezweb.ne.jp>
2026-08-27 14:54:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kakei-odyssey-v5.2-20260827';
+const CACHE='kakei-odyssey-v5.3-20260827';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
@@ -13,16 +13,12 @@ self.addEventListener('activate',e=>{
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
 self.addEventListener('fetch',e=>{
   if(e.request.mode==='navigate'){
-    e.respondWith(
-      fetch(e.request,{cache:'no-store'})
-        .then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r})
-        .catch(()=>caches.match('./index.html'))
-    );
+    e.respondWith(fetch(e.request,{cache:'no-store'})
+      .then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r})
+      .catch(()=>caches.match('./index.html')));
     return;
   }
-  e.respondWith(
-    fetch(e.request)
-      .then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r})
-      .catch(()=>caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request)
+    .then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r})
+    .catch(()=>caches.match(e.request)));
 });
